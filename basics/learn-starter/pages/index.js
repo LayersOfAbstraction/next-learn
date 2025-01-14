9ce3850db8747c7b02065ce1baf896b1b0139a69
LayersOfAbstraction <jnash51@live.com>
2025-01-14 18:11:25 +1000

    Up to https://nextjs.org/learn-pages-router/basics/data-fetching/getstaticprops-details

--- basics/learn-starter/pages/index.js
+++ basics/learn-starter/pages/index.js
@@ -1,34 +1,44 @@
 import Head from 'next/head';
 import Layout, {siteTitle} from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-
 import { getSortedPostsData } from '../lib/posts';
 
-export async function getStaticProps(){
-  return {
-    props: {
-      allPostData,
-    },
-  };
-}
-
-export default function Home() {
+export default function Home({allPostsData}) {
   return (
     <Layout home>
-    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-    <h2 className={utilStyles.headingLg}>Blog</h2>
-    <ul className={utilStyles.list}>
-      {allPostData.map(({ id, date,title }) => (
-        <li className={utilStyles.listItem} key={id}>
-          {title}
-          <br />
-          {id}
-          <br />
-          {date}
-        </li>
-      ))}
-    </ul>
-  </section>
-</Layout>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Jordan Nash</p>
+        <p>
+
+          <a href="https://nextjs.org/learn">seasoned IT Support person and aspiring developer</a>.
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
