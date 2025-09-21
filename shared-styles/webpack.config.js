5dd6ff47a2ceb951d8b93c55b9073b065f050d92
Jords <jnash51@live.com>
2025-09-21 21:29:07 +1000

    Add webpack config for css monorepo.

--- shared-styles/webpack.config.js
+++ shared-styles/webpack.config.js
@@ -1,26 +1,71 @@
-const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: 'production',
-    entry: path.resolve(__dirname, 'index.css'), 
-    output: { 
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules:[
-            {
-                test: /\.css$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                ],
+  mode: "production",
+  entry: path.resolve(__dirname, "app/App.css"),
+  entry: path.resolve(__dirname, "components/socialicons/style.css"),
+  entry: path.resolve(__dirname, "components/themetoggle/style.css"),
+  entry: path.resolve(__dirname, "header/style.css"),
+  entry: path.resolve(__dirname, "index.css"),  
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              sourceMap: false,
             },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  "postcss-flexbugs-fixes",
+                  "postcss-hover-media-feature",
+                  "postcss-nested",
+                  [
+                    "postcss-preset-env",
+                    {
+                      autoprefixer: { flexbox: "no-2009" },
+                      stage: 3,
+                      features: { "custom-properties": false },
+                    },
+                  ],
+                ],
+              },
+            },
+          },
         ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'index.css',
-        }),
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff2?|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/[hash][ext][query]",
+        },
+      },
     ],
-}
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "index.css",
+      
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".json", ".css"],
+  },
+  performance: { hints: false },
+  optimization: { runtimeChunk: false },
+};
